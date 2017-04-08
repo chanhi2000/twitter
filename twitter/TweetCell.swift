006b3dc42ccd6e57267cab2c950d8f9844a0c501
chanhi2000 <chanhi2000@gmail.com>
2017-04-08 19:15:00 +0900

    change07: gave a ui structure to tweetcell

--- twitter/TweetCell.swift
+++ twitter/TweetCell.swift
@@ -10,6 +10,33 @@ import LBTAComponents
 
 class TweetCell: DatasourceCell {
     
+    override var datasourceItem: Any? {
+        didSet {
+            guard let tweet = datasourceItem as? Tweet else {  return  }
+            
+            let attributedText = NSMutableAttributedString(string: tweet.user.name, attributes: [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 16)])
+            let usernameString = " \(tweet.user.username)\n"
+            attributedText.append(NSAttributedString(string: usernameString, attributes: [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 15), NSForegroundColorAttributeName: UIColor.gray]))
+            
+            let parargraphStyle = NSMutableParagraphStyle()
+            parargraphStyle.lineSpacing = 4
+            let range = NSMakeRange(0, attributedText.string.characters.count)
+            attributedText.addAttribute(NSParagraphStyleAttributeName, value: parargraphStyle, range: range)
+            
+            attributedText.append(NSAttributedString(string: tweet.message, attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]))
+            
+            
+            messageTextView.attributedText = attributedText
+        }
+    }
+    
+    let messageTextView: UITextView = {
+        let txtv = UITextView()
+        txtv.text = "SOME SAMPLE TEXT"
+        txtv.backgroundColor = .yellow
+        return txtv
+    }()
+    
     let profileImageView: UIImageView = {
         let imgv = UIImageView()
         imgv.image = #imageLiteral(resourceName: "zuckprofile")
@@ -28,7 +55,10 @@ class TweetCell: DatasourceCell {
         separatorLineView.backgroundColor = separatorGray
         
         addSubview(profileImageView)
+        addSubview(messageTextView)
+        
         profileImageView.anchor(topAnchor, left: leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        messageTextView.anchor(topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 4, leftConstant: 4, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
         
         
     }
